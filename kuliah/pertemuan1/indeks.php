1c0d836df207a313e86a55d5267dec4eec28ecfd
AsfiCorp <athaillahsfi@gmail.com>
2024-02-13 20:57:42 +0700

    Mengerjakan Tugas 1 PHP

---
+++ kuliah/pertemuan1/indeks.php
@@ -0,0 +1,62 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Athaillah Sulthan";
+// echo "Halo, Nama Saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y
+
+// Penggabung string / concatenation/ concat
+//.
+// $nama_depan = "Athaillah";
+// $nama_belakang = "Sulthan";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x ="7";
+// $x += "1";
+// echo $x
+// $nama = "Athaillah";
+// $nama .= " ";
+// $nama .= "Sulthan";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump (1 == 1);
+// var_dump (1 == 5);
+// var_dump (1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1")
+
+//Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0)
+ 
+
+
+
+
+?>
---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,7 @@
+<?php 
+$nama1 = "Topi";
+$nama2 = "Bundar";
+
+echo "<i>" . "<h2>" . '"'. $nama1 . " " . "saya" . " ". $nama2 . "," . " " . $nama2 . " " . $nama1 . " " . "saya." . '"' . "<i>" . "</string>"; 
+
+?>
